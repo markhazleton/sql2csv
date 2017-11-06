c21e69b77c29dc9193b8e93dbc002f926798bd6b
Mark Hazleton <mark.hazleton@gmail.com>
2017-11-06 10:22:12 -0600

    Getting project to compile

--- DBConfiguration.cs
+++ DBConfiguration.cs
@@ -1,15 +1,26 @@
-﻿public class DBConfiguration
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+
+
+
+namespace Sql2Csv
+{
+public class DbConfiguration
 {
     public string SiteName { get; set; }
     public string ConnectionString { get; set; }
-
-
-    public DBConfiguration(string DBName)
+    public DbConfiguration(string db)
     {
-        SiteName = DBName;
+        SiteName = db;
     }
-
-    public DBConfiguration()
+    public DbConfiguration()
     {
     }
 }
+}
+
+
+
