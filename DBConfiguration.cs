2a1a68c19495c60eb3518fec148290dc027d8bb8
Mark Hazleton <mark.hazleton@gmail.com>
2021-11-16 08:34:46 -0600

    clean-up

--- DBConfiguration.cs
+++ DBConfiguration.cs
@@ -1,25 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Xml.Serialization;
-using System.Xml;
 
 
 
 namespace Sql2Csv
 {
-public class DbConfiguration
-{
-    public string SiteName { get; set; }
-    public string ConnectionString { get; set; }
-    public DbConfiguration(string db)
+    public class DbConfiguration
     {
-        SiteName = db;
+        public string SiteName { get; set; }
+        public string ConnectionString { get; set; }
+        public DbConfiguration(string db)
+        {
+            SiteName = db;
+        }
+        public DbConfiguration()
+        {
+        }
     }
-    public DbConfiguration()
-    {
-    }
-}
 }
 
 
